cc81672d251d5bc328f9ae196d8f1a7d491f05f4
Leo Wilson <leowilson.xlw123@gmail.com>
2019-03-03 22:05:59 -0700

    Check credentials

--- tracker.py
+++ tracker.py
@@ -10,10 +10,10 @@ def home():
 def ping():
     return "pong"
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     # TODO: make this redirect instead of returning a value
-    if checkCred():
+    if checkCred(request.values.get('usr'), request.values.get('pass')):
         return genSess()
     else:
         return "false"
