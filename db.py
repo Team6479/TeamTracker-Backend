020026d4ced6da6e2af8d712adcca0c00d1795b6
Leo Wilson <leowilson.xlw123@gmail.com>
2019-03-06 14:12:05 -0700

    Get hashes

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 import boto3
 import os
+from boto3.dynamodb.conditions import Key, Attr
 
 aws = boto3.Session(
     aws_access_key_id=os.environ['AWS_KEY_ID'],
@@ -9,11 +10,9 @@ aws = boto3.Session(
 db = aws.resource('dynamodb')
 
 usrs = db.Table('6479-tracker-users')
-sess = db.Table('6479-tracker-sess')
+sesss = db.Table('6479-tracker-sess')
 match = db.Table('6479-tracker-match')
 pit = db.Table('6479-tracker-pit')
 
 def getPwdHash(usr):
-    # TODO: write code
-    # Below is an Argon2 hash of "password"
-    return "$argon2i$v=19$m=512,t=2,p=2$aI2R0hpDyLm3ltLa+1/rvQ$LqPKjd6n8yniKtAithoR7A"
+    return usrs.query(KeyConditionExpression=Key('usr').eq(usr))['Items'][0]['hash']
