100086dc4cd42acaaec2ee8e6ade9a459bb14257
Leo Wilson <leowilson.xlw123@gmail.com>
2019-03-08 14:51:33 -0700

    Add a logout page

--- urls.py
+++ urls.py
@@ -3,6 +3,7 @@ urls = {
     'home': BASE_URL,
     'login': {
         'page':        BASE_URL + '/login',
+        'logout':      BASE_URL + '/login?msg=logout',
         'success':     BASE_URL + '/?sess=<SESS>',
         'fail':        BASE_URL + '/login?msg=fail',
         'invalid':     BASE_URL + '/login?msg=invalid'
