a243cb408aa1dceabef6df484779f6f3154b0508
Leo Wilson <leowilson.xlw123@gmail.com>
2019-03-16 14:14:32 -0700

    Add code for match scouting

--- db.py
+++ db.py
@@ -3,6 +3,7 @@ import os
 from decimal import *
 from boto3.dynamodb.conditions import Key, Attr
 
+# AWS
 aws = boto3.Session(
     aws_access_key_id=os.environ['AWS_KEY_ID'],
     aws_secret_access_key=os.environ['AWS_KEY_SECRET'],
@@ -15,6 +16,7 @@ sesss = db.Table('6479-tracker-sess')
 match = db.Table('6479-tracker-match')
 pit = db.Table('6479-tracker-pit')
 
+# User and Login
 def getUsrInfo(usr: str):
     return usrs.query(KeyConditionExpression=Key('usr').eq(usr))['Items'][0]
 
@@ -58,3 +60,39 @@ def rmSess(sess: str):
             'sess': sess
         }
     )
+
+# Match Scouting
+def getMatchInfo(id : str):
+    return match.query(KeyConditionExpression=Key('id').eq(id))['Items'][0]
+
+def matchAdd(id: str, team: int, habStart: int, ac: int, ah: int, tha: int, thd: int, thl: int, thm: int, thh: int, tca: int, tcd: int, tcl: int, tcm: int, tch: int, habEnd: int, ct: float, c: str):
+    match.put_item(Item={
+        'id': id,
+        'team': team,
+        'auto': {
+            'habStart': habStart,
+            'cargo': ac,
+            'hatch': ah
+        },
+        'teleop': {
+            'hatch': {
+                'aq': tha,
+                'drop': thd,
+                'low': thl,
+                'mid': thm,
+                'high': thh
+            },
+            'cargo': {
+                'aq': tca,
+                'drop': tcd,
+                'low': tcl,
+                'mid': tcm,
+                'high': tch
+            }
+        },
+        'end': {
+            'habEnd': habEnd,
+            'climbTime': Decimal(ct)
+        },
+        'comments': c
+    })
---
+++ match.py
@@ -0,0 +1,20 @@
+import random, string
+import db
+
+def getInfo(id: str):
+    return db.getMatchInfo(id)
+
+def genId() -> str:
+    chars = string.ascii_letters + string.digits
+    size: int = 32
+    while(True):
+        id: str = ''.join(random.choice(chars) for i in range(size))
+        try:
+            getInfo(id)
+        except:
+            return id
+
+def add(team: int = 0, habStart: int = 1, ac: int = 0, ah: int = 0, tha: int = 0, thd: int = 0, thl: int = 0, thm: int = 0, thh: int = 0, tca: int = 0, tcd: int = 0, tcl: int = 0, tcm: int = 0, tch: int = 0, habEnd: int = 0, ct: float = 0, c: str = " "):
+    if c == "":
+        c = " "
+    db.matchAdd(genId(), team, habStart, ac, ah, tha, thd, thl, thm, thh, tca, tcd, tcl, tcm, tch, habEnd, ct, c)
