651945d470b5c8615d5fa729829c71c4be642001
Leo Wilson <42360686+leomwilson@users.noreply.github.com>
2019-03-18 11:40:52 -0700

    Make Aiden happy
    I'm sure he would really enjoy this :)

--- match.py
+++ match.py
@@ -15,6 +15,5 @@ def genId() -> str:
             return id
 
 def add(team: int = 0, habStart: int = 1, ac: int = 0, ah: int = 0, tha: int = 0, thd: int = 0, thl: int = 0, thm: int = 0, thh: int = 0, tca: int = 0, tcd: int = 0, tcl: int = 0, tcm: int = 0, tch: int = 0, habEnd: int = 0, ct: float = 0, c: str = " "):
-    if c == "":
-        c = " "
+    c = " " if c == "" else c
     db.matchAdd(genId(), team, habStart, ac, ah, tha, thd, thl, thm, thh, tca, tcd, tcl, tcm, tch, habEnd, ct, c)
